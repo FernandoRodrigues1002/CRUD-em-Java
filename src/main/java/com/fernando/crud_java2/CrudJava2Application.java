c034167b7cd8cee402dbf894dd2fda1f1f7c12e2
Nando <fernanrods1002@gmail.com>
2025-02-28 10:31:38 -0300

    att

--- src/main/java/com/fernando/crud_java2/CrudJava2Application.java
+++ src/main/java/com/fernando/crud_java2/CrudJava2Application.java
@@ -8,6 +8,12 @@ public class CrudJava2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudJava2Application.class, args);
+		
+	}
+
+	public void run(String... args) {
+		System.out.println("🚀  Aplicação CRUD em Java com Spring Boot está rodando na porta 8080! 🚀");
+		System.out.println("Acesse: http://localhost:8080/api/pessoas");
 	}
 
 }
